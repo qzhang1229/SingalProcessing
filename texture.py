4e174e2861036bdbc81049d7de054ad13217de5b
qzhang1229 <qzhang1@macalester.edu>
2021-12-16 16:45:28 -0600

    calculate entropy

--- texture.py
+++ texture.py
@@ -3,25 +3,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def entropy(signal):
-        '''
-        function returns entropy of a signal
-        signal must be a 1-D numpy array
-        '''
-        lensig=signal.size
-        symset=list(set(signal))
-        numsym=len(symset)
-        propab=[np.size(signal[signal==i])/(1.0*lensig) for i in symset]
-        ent=np.sum([p*np.log2(1.0/p) for p in propab])
-        return ent
+        len=signal.size
+        signal_val=list(set(signal))
+        propabilities=[np.size(signal[signal==i])/(1.0*len) for i in signal_val]
+        entropy=np.sum([-1.0*p*np.log2(p) for p in propabilities])
+        return entropy
 
-colorIm=Image.open('view.png')
+colorIm=Image.open('wall.png')
 greyIm=colorIm.convert('L')
 colorIm=np.array(colorIm)
 greyIm=np.array(greyIm)
 
-N=5
+N=3
 S=greyIm.shape
-E=np.array(greyIm)
+Entropy=np.array(greyIm)
 for row in range(S[0]):
         for col in range(S[1]):
                 Lx=np.max([0,col-N])
@@ -29,7 +24,7 @@ for row in range(S[0]):
                 Ly=np.max([0,row-N])
                 Uy=np.min([S[0],row+N])
                 region=greyIm[Ly:Uy,Lx:Ux].flatten()
-                E[row,col]=entropy(region)
+                Entropy[row,col]=entropy(region)
 
 plt.subplot(1,3,1)
 plt.imshow(colorIm)
@@ -38,8 +33,8 @@ plt.subplot(1,3,2)
 plt.imshow(greyIm, cmap=plt.cm.gray)
 
 plt.subplot(1,3,3)
-plt.imshow(E, cmap=plt.cm.jet)
-plt.xlabel('Entropy in 10x10 neighbourhood')
+plt.imshow(Entropy, cmap=plt.cm.jet)
+plt.xlabel('Entropy in 6x6 neighbourhood')
 plt.colorbar()
 
 plt.show()
